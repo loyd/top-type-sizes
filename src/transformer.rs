bb64621933edf67b97aaaae2e8feef869d8fa12d
Paul Loyd <pavelko95@gmail.com>
2023-02-19 18:23:19 +0800

    refactor(transformer): run per type transformations

--- src/transformer.rs
+++ src/transformer.rs
@@ -6,14 +6,22 @@ use crate::{options::Options, schema::*};
 // TODO: merge types with common prefix and similar layouts.
 // TODO: support whitelist and blacklist.
 
-fn merge_variants(variants: Vec<EnumVariant>) -> Vec<EnumVariant> {
-    if variants.len() <= 1 {
-        return variants;
+/// Sorts all variants and merges ones with similar layouts.
+fn sort_and_merge_variants(type_: &mut Type) {
+    let TypeKind::Enum(e) = &mut type_.kind else {
+        return;
+    };
+
+    if e.variants.len() <= 1 {
+        return;
     }
 
-    let mut new = Vec::with_capacity(variants.len());
+    let mut old = std::mem::take(&mut e.variants);
+    old.sort_by_key(|v| Reverse(v.size)); // TODO: sort also by hash of items.
 
-    for var in variants {
+    let mut new = Vec::with_capacity(old.len());
+
+    for var in old {
         if let Some(EnumVariant { name, size, items }) = new.last_mut() {
             if var.size == *size && &var.items == items {
                 name.push_str(", ");
@@ -25,30 +33,19 @@ fn merge_variants(variants: Vec<EnumVariant>) -> Vec<EnumVariant> {
         new.push(var);
     }
 
-    new
-}
-
-/// Sorts all variants and merges ones with similar layouts.
-fn sort_and_merge_variants(types: &mut [Type]) {
-    types
-        .iter_mut()
-        .filter_map(|type_| match &mut type_.kind {
-            TypeKind::Enum(e) => Some(&mut e.variants),
-            TypeKind::Struct(_) => None,
-        })
-        .for_each(|variants| {
-            variants.sort_by_key(|v| Reverse(v.size));
-            *variants = merge_variants(std::mem::take(variants));
-        })
+    e.variants = new;
 }
 
 pub fn transform(mut types: Vec<Type>, options: &Options) -> Vec<Type> {
     // Use stable sort to preserve partial ordering.
+    // Also sort by name to do proper deduplication.
     types.sort_by(|a, b| (b.size, &b.name).cmp(&(a.size, &a.name)));
     types.dedup();
     types.truncate(options.limit);
 
-    sort_and_merge_variants(&mut types);
+    for type_ in &mut types {
+        sort_and_merge_variants(type_);
+    }
 
     types
 }
