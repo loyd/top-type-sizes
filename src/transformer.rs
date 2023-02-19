9ba36212557c7fba2ff348b3e61a32d2570189ad
Paul Loyd <pavelko95@gmail.com>
2023-02-19 09:49:39 +0800

    feat(transformer): dedup types

--- src/transformer.rs
+++ src/transformer.rs
@@ -1,5 +1,3 @@
-use std::cmp::Reverse;
-
 use crate::{options::Options, schema::*};
 
 // TODO: merge variants with same layout.
@@ -7,7 +5,8 @@ use crate::{options::Options, schema::*};
 // TODO: link large types (parent-child).
 
 pub fn transform(mut types: Vec<Type>, options: &Options) -> Vec<Type> {
-    types.sort_unstable_by_key(|t| Reverse(t.size));
+    types.sort_unstable_by(|a, b| (b.size, &b.name).cmp(&(a.size, &a.name)));
+    types.dedup();
     types.truncate(options.limit);
     types
 }
