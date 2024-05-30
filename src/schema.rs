419663190ae4010785b585b8d965ab339e8ee67c
Paul Loyd <pavelko95@gmail.com>
2024-05-30 13:13:51 +0400

    test: add output snaps & use yaml for repr snaps

--- src/schema.rs
+++ src/schema.rs
@@ -1,37 +1,44 @@
-#[derive(Debug, PartialEq, Eq)]
+use serde::Serialize;
+
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub struct Type {
     pub name: String,
     pub size: usize,
     pub align: usize,
     pub kind: TypeKind,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub end_padding: Option<usize>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub enum TypeKind {
     Struct(StructType),
     Enum(EnumType),
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub struct StructType {
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub items: Vec<FieldOrPadding>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub struct EnumType {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub discriminant_size: Option<usize>,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub variants: Vec<EnumVariant>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub struct EnumVariant {
     pub name: String,
     pub size: usize,
+    #[serde(skip_serializing_if = "Vec::is_empty")]
     pub items: Vec<FieldOrPadding>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub enum FieldOrPadding {
     Field(Field),
     Padding(usize),
@@ -46,17 +53,20 @@ impl FieldOrPadding {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub struct Field {
     pub kind: FieldKind,
     pub name: String,
     pub size: usize,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub align: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub offset: Option<usize>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub local_type: Option<String>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub enum FieldKind {
     AdtField,
     Upvar,
--- tests/runner.rs
+++ tests/runner.rs
@@ -1,21 +1,55 @@
+use structopt::StructOpt;
+
 use top_type_sizes::*;
+
+fn test(content: &'static str) {
+    let refined_content = reader::read(content.as_bytes()).unwrap();
+
+    let types = parser::parse(&refined_content).unwrap();
+    insta::with_settings!({ description => "internal representation" }, {
+        insta::assert_yaml_snapshot!("types", types);
+    });
+
+    snap_output(&types, &[]);
+    snap_output(&types, &["-ws"]);
+    snap_output(&types, &["-h8"]);
+    snap_output(&types, &["-ws", "-h16"]);
+}
+
+fn snap_output(types: &[schema::Type], cmd: &[&str]) {
+    let full_cmd = std::iter::once(&"top-type-sizes")
+        .chain(cmd)
+        .copied()
+        .collect::<Vec<_>>();
+
+    let options = options::Options::from_iter(&full_cmd);
+    let snap_name = {
+        let mut parts = cmd.to_vec();
+        parts.insert(0, "output");
+        parts.join("")
+    };
+
+    let types = transformer::transform(types.to_vec(), &options);
+    let output = formatter::format(types, &options);
+
+    insta::with_settings!({ description => full_cmd.join(" ") }, {
+        insta::assert_snapshot!(snap_name, output);
+    })
+}
 
 macro_rules! test_sample {
     ($name:ident) => {
         #[test]
         fn $name() {
-            static SAMPLE: &'static str =
-                include_str!(concat!("samples/", stringify!($name), ".txt"));
-
-            let refined_sample = reader::read(SAMPLE.as_bytes()).unwrap();
-            let types = parser::parse(&refined_sample).unwrap();
-            insta::assert_debug_snapshot!(format!("{}_types", stringify!($name)), types);
-
-            // TODO: check the transformer.
-
-            let output = formatter::format(types, &Default::default());
-            println!("{output}");
-            // TODO: make snapshots of output.
+            insta::with_settings!(
+            {
+                snapshot_path => concat!("snapshots/", stringify!($name)),
+                prepend_module_to_snapshot => false,
+                omit_expression => true,
+            },
+            {
+                test(include_str!(concat!("samples/", stringify!($name), ".txt")))
+            })
         }
     };
 }
